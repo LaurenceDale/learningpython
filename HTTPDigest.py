b75d0b18998d9841f0eb8ab2e2e7cca99fe4214b
Lightraine2 <laurencedale1986@gmail.com>
2017-08-12 12:35:35 +0100

    Add files via upload

---
+++ HTTPDigest.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+#HTTP Digest authentication script.
+#Digest authentication works as follows: hash1 is MD5 a hash of the inputs ('username:url:password')
+# hash2 is MD5 hash of (method:uri)
+# the response hash is MD5 of (hash1:nonce:hash2) where the nonce is a unique value provided by server. Session ID esque
+
+import hashlib
+
+hash1 = hashlib.md5('admin:PentesterAcademy:asdds').hexdigest()
+
+hash2 = hashlib.md5('GET:/lab/webapp/digest/1').hexdigest()
+
+response_string = hash1 + ':' + nonce + ':' + hash2
+
+response = hashlib.md5(response_string).hexdigest()
