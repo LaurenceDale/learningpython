697e4db10feae1eb9a7c2d9a9080c6ba6ec91219
Lightraine2 <laurencedale1986@gmail.com>
2017-11-25 21:13:32 +0000

    Update investment.py

--- investment.py
+++ investment.py
@@ -18,7 +18,13 @@ if objective == "1":
         time.sleep(1)
         print "Firstly, let's start by getting to know your circumstances a little better. This will help me give you the best advice possible."
         time.sleep(1)
-        print "How much capital do you have to invest?"
-
+        print "How much income do you require from your investment?\n"
+        print "(in practicality, we'll link this in from the 'fact find' process)\n"
+        time.sleep(1)
+        incomegoal = raw_input("Enter income required in £'s: ")
+        time.sleep(1)
+        print "Ok, and when is this income required?\n"
+        print "Enter ammont of years until income is needed. (0 - 20)\n"
+        incometerm = raw_input("Enter number of years for investment: ")
 else:
         print "Ok, let's take a look at Capital Growth options."
